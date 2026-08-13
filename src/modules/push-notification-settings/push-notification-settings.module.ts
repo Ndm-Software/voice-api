d8bbfac6057bdc1f2cebe80ce06fca7385222832
yagmurylmaz <yagmurylmaaz@hotmail.com>
2026-08-13 14:21:39 +0300

    fix: prisma module dependency and typescript config

--- src/modules/push-notification-settings/push-notification-settings.module.ts
+++ src/modules/push-notification-settings/push-notification-settings.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { PushNotificationSettingsController } from './push-notification-settings.controller';
 import { PushNotificationSettingsService } from './push-notification-settings.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PushNotificationSettingsController],
   providers: [PushNotificationSettingsService],
   exports: [PushNotificationSettingsService],
